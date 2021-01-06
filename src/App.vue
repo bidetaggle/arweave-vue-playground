cc32a2cad6125a39fecd743df10bba7a9ba1c702
bidetaggle <bidetaggle@mac>
2021-01-06 14:29:05 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <img
+    alt="logo"
+    src="https://arweave.net/lIXEwj6WL4V1ZR_hJjlhsziZp4IVnJQS52rpD0xOvJ4"
+  />
+  <form v-on:submit.prevent="search">
+    <input
+      name="transaction_id"
+      v-model.trim="searchBar"
+      placeholder="Type a transaction id"
+    />
+  </form>
+  <transaction :data="transaction" :tags="tags" />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Transaction from "./components/Transaction.vue";
+import Arweave from "arweave";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Transaction
+  },
+  data() {
+    return {
+      searchBar: "hKMMPNh_emBf8v_at1tFzNYACisyMQNcKzeeE1QE9p8",
+      transaction: {},
+      tags: {}
+    };
+  },
+  methods: {
+    async getTransaction(_id) {
+      const arweave = Arweave.init();
+      arweave.transactions
+        .get(_id)
+        .then(res => {
+          this.transaction = {
+            id: res.id,
+            last_tx: res.last_tx,
+            quantity: res.quantity,
+            data_size: res.data_size
+          };
+          this.tags = res.tags;
+        })
+        .catch(() => {
+          this.transaction = {};
+        });
+    },
+    search(e) {
+      console.log(e.target.transaction_id.value);
+      this.getTransaction(e.target.transaction_id.value);
+    }
   }
 };
 </script>
@@ -22,5 +63,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+img {
+  width: 200px;
+  margin-top: -120px;
+  margin-bottom: 30px;
+}
+
+table {
+  width: 500px;
+  border-collapse: collapse;
+  text-align: left;
+  font-family: monospace;
+}
+td,
+th {
+  border: 1px solid black;
 }
 </style>
---
+++ src/components/Transaction.vue
@@ -0,0 +1,58 @@
+<template>
+  <h2>Transaction # {{ data.id }}</h2>
+  <table v-if="data.id">
+    <thead>
+      <tr>
+        <th>key</th>
+        <th>value</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in Object.entries(data)" :key="item">
+        <td>{{ item[0] }}</td>
+        <td>{{ item[1] }}</td>
+      </tr>
+      <tr>
+        <td>Tags</td>
+        <td>
+          <tr>
+            <th>name</th>
+            <th>value</th>
+          </tr>
+          <tr v-for="tag in tags" :key="tag">
+            <td>{{ tag.name }}</td>
+            <td>{{ tag.value }}</td>
+          </tr>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "Transaction",
+  props: {
+    data: Object,
+    tags: Object
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
